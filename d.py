a38ec47fd6eaca83a9386a54569ece31fea6d70a
filip lorenc <fifa.lorenc08@gmail.com>
2024-03-19 09:29:58 +0100

    hggt

---
+++ d.py
@@ -0,0 +1,4 @@
+auta = ["1:ford", "2:porsche", "3:audi", "4:bmw", "5:mercedes", "6:skoda"]
+
+for x in auta:
+    print(x)
---
+++ pole.py
@@ -0,0 +1,14 @@
+print("Szukam dzieci w sklepie")
+
+cislo = int(1)
+cislo_float = float(3.22)
+text = str("string je sada znaků, např: pro text")
+boolean = True/False
+
+#datový typ - pole, kde muzeme mit vic prvku. zavorky = pravy alt + f
+#poly se zacina na pozici 0. ackoliv je delka pole 6, posledni prvek je na pozici 5
+
+            #0       #1     #2      #3      #4      #5
+
+arrays = ["ford", "porsche", "audi", "bmw", "mercedes", "skoda"]
+print (arrays)
